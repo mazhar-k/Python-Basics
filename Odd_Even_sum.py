b6e3834f45e40b6fcd91e43498a7dcad44b3e7c7
mazhar-k <64404507+mazhar-k@users.noreply.github.com>
2020-08-16 18:43:32 +0530

    Update Odd_Even_sum.py
    Updated to a shorter and easy version of code

--- Odd_Even_sum.py
+++ Odd_Even_sum.py
@@ -2,6 +2,6 @@ even = [0, 2, 4, 6, 8]
 odd = [1, 3, 5, 7, 9]
 result = []
 for e,o in zip(even,odd):
-    temp = e + o
-    result.append(temp)
+    tem = e + o
+    result.append(tem)
 print(result)
