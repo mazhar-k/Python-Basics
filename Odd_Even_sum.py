79f17016580edba50986e3f35a2eca9b35817f3f
mazhar-k <64404507+mazhar-k@users.noreply.github.com>
2020-08-16 18:27:40 +0530

    Update Odd_Even_sum.py

--- Odd_Even_sum.py
+++ Odd_Even_sum.py
@@ -1,7 +1,7 @@
-list1 = [0, 2, 4, 6, 8]
-list2 = [1, 3, 5, 7, 9]
-list3 = list1 + list2
-sum = 0
-for x in range(0, len(list3)):
-    sum += list3[x]
-print("Sum of elements of list1 and list2 is: ", sum)
+even = [0, 2, 4, 6, 8]
+odd = [1, 3, 5, 7, 9]
+result = []
+for e,o in zip(even,odd):
+    temp = e + o
+    result.append(temp)
+print(result)
